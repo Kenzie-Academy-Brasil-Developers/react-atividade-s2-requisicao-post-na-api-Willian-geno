1fd4abf523a08aec622193ee276750d4eb773f5b
Willian <willian@pop-os.localdomain>
2021-09-28 14:45:25 -0300

    atualizando aplicação

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,41 +1,49 @@
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup"
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import axios from 'axios';
 import "./Login.css"
 
 const Login = () =>{
-    const [respostaApi, setRespostaApi] = useState();
-    const [date, setDate] = useState();
+    const [respostaApi, setRespostaApi] = useState(false);
+    const [date, setDate] = useState({});
 
-    const formShema = yup.object().shape({
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+   /*  const formShema = yup.object().shape({
         username: yup.string(),
         password: yup.string()
-    });
+    }); */
 
-    const {register, handleSubmit, formState: {errors}} =useForm({
+/*     const {register, handleSubmit, formState: {errors}} =useForm({
         resolver: yupResolver(formShema)
-    })
+    }) */
 
-    const hendleform = (date) => setDate(date);
-    
+    useEffect(() => {
+        setDate({ username: username, password: password})
+    }, [username, password])
 
-    axios.post("https://kenzieshop.herokuapp.com/sessions/", date)
+    const hendleform = () => {
+            
+        axios.post("https://kenzieshop.herokuapp.com/sessions/", date)
         .then((response)=>{
             setRespostaApi("Requisição Completa")
-        })
-        .catch((err) => { 
-            setRespostaApi("Requisição Falhou")
-        });
-
+            /* setDate(date) */
+            })
+            .catch((err) => { 
+                setRespostaApi("Requisição Falhou")
+            });
+    };
+    
 
     return <div className = "divInput" >
-            <form onSubmit = {handleSubmit(hendleform)}>
-                <input type="text" placeholder = "username" {...register("username")}/>
-                <input type="text" placeholder = "password" {...register("password")} />
-                <button type = "subimt">enviar</button>
-            </form>
+            <div /*  onSubmit = {handleSubmit(hendleform)} */>
+                <input type="text" value = {username} onChange = {(event) => setUsername(event.target.value)} placeholder = "username" /* {...register("username") }*//>
+                <input type="text" valeu = {password} onChange = {(event) => setPassword(event.target.value)} placeholder = "password" /* {...register("password")} */ />
+                <button  onClick = {hendleform} >enviar</button>
+            </div>
             <h1>{respostaApi}</h1>
         </div> 
 }
